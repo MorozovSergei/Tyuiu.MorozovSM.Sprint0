c468ab3636aeff4e26a22af374dee18ecef4c8d1
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2024-09-11 21:34:33 +0500

    Task1

---
+++ Tyuiu.MorozovSM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.MorozovSM.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Никто";
+            int age = 45;
+            Console.WriteLine("Здравствуйте, меня " + name + " не звал и мне небыло " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
