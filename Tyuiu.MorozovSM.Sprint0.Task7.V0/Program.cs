729729fd9b655f275108c272ae0031387e1fff0b
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2024-09-13 20:37:01 +0500

    Task7

---
+++ Tyuiu.MorozovSM.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,58 @@
+﻿using Tyuiu.MorozovSM.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.MorozovSM.Sprint0.Task7.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил: Морозов С. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Морозов Сергей Максимович | АСОиУб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+            Console.WriteLine("* одиныковых массивов по длинне.                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №1");
+            foreach(int i in arrayNums1)
+            {
+                Console.Write(i+", ");
+            }
+            Console.WriteLine();
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №1");
+            foreach (int i in arrayNums2)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (arrayNums1.Length ==  arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1,arrayNums2);
+                Console.WriteLine("Сумма элементов массива ровна:");
+                foreach (int i in resultArray)
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные массивы имеют разное количество элементов");
+            }
+            Console.ReadKey();
+        }
+    }
+}
